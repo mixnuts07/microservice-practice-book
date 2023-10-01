de3b06e7d7cf6bc5442e8d69d99b425a71199741
mixnuts07 <french0719@gmail.com>
2023-10-01 17:36:21 +0900

    add catalog

--- catalog/README.md
+++ catalog/README.md
@@ -1,5 +1,5 @@
 ```bash
 go mod init <project-name> ... 新しいプロジェクトの作成とgo.modファイルを作る.
 go run main.go ... プログラムの実行
-
+go get github.com/hoge/fuga@latest...依存関係の追加。依存関係が追加されるとgo.sumファイルが作成され.
 ```
--- catalog/main.go
+++ catalog/main.go
@@ -1,6 +1,36 @@
+// 実行可能なプログラムのエントリーポイントがmainパッケージになる.つまりmainパッケージにはmain()関数が必要
 package main
 
 import "fmt"
+
+// 書籍情報の構造体
+type Book struct {
+	ID int
+	Title string
+	Author string
+	Price int
+}
+
+// DBを使わずに書籍情報を返せるように構造体にデータを保持
+var (
+	book1 = Book{
+		ID: 1,
+		Title: "Go言語プログラミング",
+		Author: "山田太郎",
+		Price: 3600,
+	}
+	book2 = Book{
+		ID: 2,
+		Title: "Go言語でつくるインタプリタ",
+		Author: "山田太郎2",
+		Price: 36000,
+	}
+	books = []Book{book1, book2}
+)
+
+func getBooks(i int32) Book {
+	return books[i-1]
+}
 
 func main() {
 	fmt.Println("Hello, World!")
